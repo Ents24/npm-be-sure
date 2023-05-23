1f65ea2a55fb10e8acc72d94cb23c5660a0989b3
Xander <xander.king@ents24.com>
2023-05-23 13:44:36 +0100

    Added `errorType` / `errorText` params for more control at call time.
    Separate params were preferred to
     a) A callback, because this lib is supposed to be "throwy"
     b) A pre-built error object, because that would involve constructing
        an error every time and then ignoring it for valid cases. Also the
        default error message would need to be tacked on where no msg exists
        which is a bit nasty.
    
    Also added decoration for strings to avoid obscure error msgs

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const { getValidator, setValidator } = require('./src/validators.js');
  * Find a validator that matches `key`, run it with the passed `value`, throw on failure
  * @param {Mixed} value the value to check
  * @param {String} key the validator key to check against
+ * @param {Type | undefined} errorType a type that extends Error
+ * @param {String | undefined} errorText the text to include if an error is thrown
  * @throws A validator matching the passed key must exist and return true
  */
-module.exports.beSure = (value, key) => {
+module.exports.beSure = (value, key, errorType, errorText) => {
   const validator = getValidator(key)
 
   if(typeof validator !== 'function') {
@@ -16,9 +18,21 @@ module.exports.beSure = (value, key) => {
 
   const ok = validator(value)
 
-  if (ok !== true) {
-    throw new ValidationError(`${value} failed validation for: "${key}"`)
+  if (ok === true) {
+    return
   }
+
+  // Apply default error text if necessary
+  if (typeof errorText !== 'string') {
+    errorText = `${decorateValue(value)} failed validation for: ${key}`
+  }
+
+  // Apply default error type if necessary (JS classes are constructor functions)
+  if (typeof errorType !== 'function' || errorType.prototype instanceof Error === false) {
+    errorType = ValidationError
+  }
+
+  throw new errorType(errorText)
 }
 
 /**
@@ -26,24 +40,20 @@ module.exports.beSure = (value, key) => {
  * Never throw on `null` value
  * @param {Mixed} value the value to check
  * @param {String} key the validator key to check against
+ * @param {Type | undefined} errorType a type that extends Error
+ * @param {String | undefined} errorText the text to include if an error is thrown
  * @throws A validator matching the passed key must exist and return true
  */
-module.exports.beSureAllowNull = (value, key) => {
-  const validator = getValidator(key)
-
-  if(typeof validator !== 'function') {
-    throw `No validator found for key: ${key}`
-  }
-
-  if(value === null) {
-    return true
-  }
-
-  const ok = validator(value)
-
-  if (ok !== true) {
-    throw new ValidationError(`${value} failed validation for: "${key}"`)
+module.exports.beSureAllowNull = (value, key, errorType, errorText) => {
+  if (value !== null) {
+    this.beSure(value, key, errorType, errorText)
   }
 }
+
+/**
+ * Decorate a value to make log output more readable. Currently just wraps
+ * strings in quotes
+ */
+decorateValue = value => typeof value === 'string' ?  `'${value}'` : value;
 
 module.exports.setValidator = setValidator
